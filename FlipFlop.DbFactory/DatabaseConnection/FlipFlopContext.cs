d42bb2e4de3b6313e9c96c7f5e87d99037a9aa6b
shivam kumar <shivamkumar683@gmail.com>
2025-04-20 00:12:49 +0530

    DbConnection

---
+++ FlipFlop.DbFactory/DatabaseConnection/FlipFlopContext.cs
@@ -0,0 +1,36 @@
+﻿using FlipFlop.Models.DBModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipFlop.DbFactory.DatabaseConnection
+{
+    public class FlipFlopContext : IdentityDbContext<ApplicationUser,IdentityRole,string>
+    {
+        public FlipFlopContext(DbContextOptions<FlipFlopContext> option): base(option)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("dbo");
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+           
+    }
+
+
+}
---
+++ FlipFlop.Models/DBModel/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipFlop.Models.DBModel
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public string? RefreshToken { get; set; }
+        public string? CompanyName { get; set; }
+    }
+}
